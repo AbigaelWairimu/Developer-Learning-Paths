478776a06514ef4d5d0dbcd6b3d34a34473ec8f1
Abby Maribe <maribeabby@gmail.com>
2024-05-09 19:58:57 +0300

    Added config flie

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    pages: {
+      '/frontend': {
+        entry: 'src/pages/frontend.html'
+      },
+      '/backend': {
+        entry: 'src/pages/backend.html'
+      },
+      '/communities': {
+        entry: 'src/pages/communities.html'
+      },
+      '/contact': {
+        entry: 'src/pages/contact.html'
+      },
+      '/guides': {
+        entry: 'src/pages/guides.html'
+      },
+      '/videos': {
+        entry: 'src/pages/videos.html'
+      }
+    }
+  }
