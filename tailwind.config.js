26c0d3387301538570c29ab5583c39bb953db9e2
Abby Maribe <maribeabby@gmail.com>
2024-05-09 12:36:48 +0300

    Added content to backend.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    "./index.html", "./frontend.html", "./backend.html",
     "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
