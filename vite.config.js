28cdd0b6e885da168618931c496eb625a6340dad
Abby Maribe <maribeabby@gmail.com>
2024-05-10 21:49:49 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
+
+import Inspect from "vite-plugin-inspect";
+import { resolve } from "path";
+
 export default {
-    build: {
-      rollupOptions: {
-        input: {
-          frontend: 'src/pages/frontend.html',
-          backend: 'src/pages/backend.html',
-          communities: 'src/pages/communities.html',
-          contact: 'src/pages/contact.html',
-          guides: 'src/pages/guides.html',
-          videos: 'src/pages/videos.html'
-        }
+  plugins: [Inspect()],
+  build: {
+    rollupOptions: {
+      input: {
+         main: resolve(__dirname, "index.html"),
+         backend: resolve(__dirname, "src/pages/backend.html"),
+         frotend: resolve(__dirname, "src/pages/frontend.html"),
+         communities: resolve(__dirname, "src/pages/communities.html"),
+         contact: resolve(__dirname, "src/pages/contact.html"),
+         guides: resolve(__dirname, "src/pages/guides.html"),
+         videos: resolve(__dirname, "src/pages/videos.html"),
+         404: resolve(__dirname, "src/pages/404.html"),
       }
-    }
+    } 
   }
+}
