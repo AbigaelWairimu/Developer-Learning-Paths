1f6aba40e7464e920000da7fa3d16ac41f7393c7
Abby Maribe <maribeabby@gmail.com>
2024-05-11 00:43:26 +0300

    Added a html page

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default {
          contact: resolve(__dirname, "src/pages/contact.html"),
          guides: resolve(__dirname, "src/pages/guides.html"),
          videos: resolve(__dirname, "src/pages/videos.html"),
+         devops: resolve(__dirname, "src/pages/devops.html"),
          404: resolve(__dirname, "src/pages/404.html"),
       }
     } 
