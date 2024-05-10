c009fd10d0cfcff3558cd13eba436e4182d92b69
Abby Maribe <maribeabby@gmail.com>
2024-05-11 02:05:57 +0300

    Added more html pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", "src/pagesfrontend.html", "src/pages/backend.html", "src/pages/communities.html", "src/pages/guides.html", "src/pages/404.html", "src/pages/videos.html", "src/pages/contact.html", "src/pages/devops.html",
+    "./index.html", "src/pagesfrontend.html", "src/pages/backend.html", "src/pages/communities.html", "src/pages/guides.html", "src/pages/404.html", "src/pages/videos.html", "src/pages/contact.html", "src/pages/devops.html", "src/pages/ai.html", "src/pages/datascience.html", "src/pages/machinelearning.html", "src/pages/cybersecurity.html", "src/pages/blockchain.html", "src/pages/flutter.html", "src/pages/gamedev.html", "src/pages/projectmanagement.html", "src/pages/uxdesign.html", "src/pages/software-architect.html", "src/pages/react.html", "src/pages/computer-science.html", "src/pages/python.html", "src/pages/java.html", "src/pages/javascript", "src/pages/angular.html", "src/pages/node.html",
     "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 
 import Inspect from "vite-plugin-inspect";
 import { resolve } from "path";
+import { machine } from "os";
 
 export default {
   plugins: [Inspect()],
@@ -16,6 +17,24 @@ export default {
          videos: resolve(__dirname, "src/pages/videos.html"),
          devops: resolve(__dirname, "src/pages/devops.html"),
          404: resolve(__dirname, "src/pages/404.html"),
+         ai: resolve(__dirname, "src/pages/ai.html"),
+         datascience: resolve(__dirname, "src/pages/datascience.html"),
+         machinelearning: resolve(__dirname, "src/pages/machinelearning.html"),
+         cybersecurity: resolve(__dirname, "src/pages/cybersecurity.html"),
+         blockchain: resolve(__dirname, "src/pages/blockchain.html"),
+         flutter: resolve(__dirname, "src/pages/flutter.html"),
+         gamedev: resolve(__dirname, "src/pages/gamedev.html"),
+         projectmanagement: resolve(__dirname, "src/pages/projectmanagement.html"),
+         uxdesign: resolve(__dirname, "src/pages/uxdesign.html"),
+        softwareArchitect: resolve(__dirname, "src/pages/software-architect.html"),
+        react: resolve(__dirname, "src/pages/react.html"),
+        computerScience: resolve(__dirname, "src/pages/computer-science.html"),
+        python: resolve(__dirname, "src/pages/python.html"),
+        java: resolve(__dirname, "src/pages/java.html"),
+        javascript: resolve(__dirname, "src/pages/javascript.html"),
+        angular: resolve(__dirname, "src/pages/angular.html"),
+        node: resolve(__dirname, "src/pages/node.html"),
+
       }
     } 
   }
